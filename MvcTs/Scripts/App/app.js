f8b503379c2b6e9fd33ce12729ef3232e17078e1
gontival <joel.levant@gmail.com>
2017-01-13 17:08:01 +0100

    Initial Commit
    packages missing

---
+++ MvcTs/Scripts/App/app.js
@@ -0,0 +1,9 @@
+///// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
+function sayHello() {
+    //$(document).ready(function () {
+    var compiler = document.getElementById("compiler").value;
+    var framework = document.getElementById("framework").value;
+    return "Hello from " + compiler + " and " + framework + "!";
+    //});
+}
+//# sourceMappingURL=app.js.map
---
+++ MvcTs/src/app.ts
@@ -0,0 +1,9 @@
+﻿///// <reference path="../Scripts/typings/jquery/jquery.d.ts"/>
+
+function sayHello() {
+    //$(document).ready(function () {
+        const compiler = (document.getElementById("compiler") as HTMLInputElement).value;
+        const framework = (document.getElementById("framework") as HTMLInputElement).value;
+        return `Hello from ${compiler} and ${framework}!`;
+    //});
+}
